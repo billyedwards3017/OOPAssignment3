e2dd955c029a3e16f36690f39272d85bf72cd1af
billyedwards3017 <43846656+billyedwards3017@users.noreply.github.com>
2020-04-17 20:37:00 +0100

    added the check for difference in lines

--- Program.cs
+++ Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
 
 namespace OOPAssignment3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int Choice1;
+            string temp;
+            int Choice2;
+            Dictionary<int, string> GitRepos = new Dictionary<int, string>();
+            //Declaring the variables required for the program
+
+            GitRepos.Add(1, "GitRepositories_1a.txt");
+            GitRepos.Add(2, "GitRepositories_1b.txt");
+            GitRepos.Add(3, "GitRepositories_2a.txt");
+            GitRepos.Add(4, "GitRepositories_2b.txt");
+            GitRepos.Add(5, "GitRepositories_3a.txt");
+            GitRepos.Add(6, "GitRepositories_3b.txt");
+            //This adds the key and value pairs into the dictionary for the viable text documents
+
+            foreach (KeyValuePair<int, string> item in GitRepos)
+            {
+                Console.WriteLine("Please enter {0} if you want to use {1}", item.Key, item.Value);
+            }
+            //This will write to the console each option of text document that the user can choose from, allowing new text docuemtns to be added easily
+            do
+            {
+                Console.WriteLine("Input the value corrosponding to the first text file you would like to compare");
+                temp = (Console.ReadLine());
+                int.TryParse(temp, out Choice1);
+            } while (!(Choice1 == 1 || Choice1 == 2 || Choice1 == 3 || Choice1 == 4 || Choice1 == 5 || Choice1 == 6));
+            //This will take the users input in the first text document that they will want to use. This also includes a error handle which means the only viable inputs are the displayed options
+            do
+            {
+                Console.WriteLine("Input the value corrosponding to the second text file you would like to compare");
+                temp = (Console.ReadLine());
+                int.TryParse(temp, out Choice2);
+            } while (!(Choice2 == 1 || Choice2 == 2 || Choice2 == 3 || Choice2 == 4 || Choice2 == 5 || Choice2 == 6));
+            //this does the same as the above code but for the second option
+            string Git1a = File.ReadAllText(Convert.ToString(GitRepos[Choice1]));
+            string Git1b = File.ReadAllText(Convert.ToString(GitRepos[Choice2]));
+            //This will extract the text from the chosen text file into string variables
+            if (Git1a == Git1b)
+            {
+                Console.WriteLine($"{GitRepos[Choice1]} and {GitRepos[Choice2]} are not different.");
+            }
+            else
+            {
+
+                Console.WriteLine($"{GitRepos[Choice1]} & {GitRepos[Choice2]} are different.");
+                //this section of code compares the text of each file and if there are no differences, it will write to the console there are no diferences and vice versa
+
+                string[] lines1 = Git1a.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                string[] lines2 = Git1b.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                //this section of code will create arrays of the text in the files with each item of the array being a line of the file
+                List <int> difflines = new List <int>();
+                //this line of code creates a list which will contain the lines in the files which have differences 
+                for (int x = 0; x <= lines1.Length-1; x++)
+                {
+                    if (lines1[x] != lines2[x])
+                    {
+                        difflines.Add(x); 
+                    }
+                }
+                //this for loop will actually compare each line of the two text files against eachother and add the line number to the list if there is a difference
+
+                Console.WriteLine("You have differences on the following lines:");
+                
+                foreach (int diffline in difflines)
+                { 
+                    Console.WriteLine(diffline);
+                }
+                //this foreach loop will write each line number entered into the difflines list
+
+
+
+
+
+               
+            }
+           
+            Console.ReadKey();
         }
     }
 }
